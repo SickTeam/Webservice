6eef5a71c5ec418b4a6a31cb46c15ce5f1b6287a
MikaelEC <mikaelec@gmail.com>
2015-05-01 16:55:01 +0200

    Implemented new RAML file

--- Webservice/Controllers/GamesController.cs
+++ Webservice/Controllers/GamesController.cs
@@ -6,24 +6,39 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Webservice.ApiV1.Models;
 
-namespace Webservice.ApiV1 {
-  public partial class GamesController : IGamesController {
+namespace Webservice.ApiV1
+{
+    public partial class GamesController : IGamesController
+    {
 
-    /// <param name="gamespostrequestcontent"></param>
-    /// <returns>GamesPostOKResponseContent</returns>
-    public IHttpActionResult Post(Models.GamesPostRequestContent gamespostrequestcontent) {
-      // TODO: implement Post - route: games/
-      // var result = new GamesPostOKResponseContent();
-      // return Ok(result);
-      return Ok();
+				/// <param name="gamespostrequestcontent"></param>
+		/// <returns>GamesPostOKResponseContent</returns>
+        public IHttpActionResult Post(Models.GamesPostRequestContent gamespostrequestcontent)
+        {
+            // TODO: implement Post - route: games/
+			// var result = new GamesPostOKResponseContent();
+			// return Ok(result);
+			return Ok();
+        }
+
+				/// <param name="gameidplayerspostrequestcontent"></param>
+		/// <param name="gameId"></param>
+        public IHttpActionResult PostByGameIdPlayers(Models.GameIdPlayersPostRequestContent gameidplayerspostrequestcontent,[FromUri] string gameId)
+        {
+            // TODO: implement PostByGameIdPlayers - route: games/{gameId}/players
+			return Ok();
+        }
+
+		/// <summary>
+		/// Kick a player
+		/// </summary>
+		/// <param name="playerId"></param>
+		/// <param name="gameId"></param>
+        public IHttpActionResult Delete([FromUri] string playerId,[FromUri] string gameId)
+        {
+            // TODO: implement Delete - route: games/{gameId}/players/{playerId}
+			return Ok();
+        }
+
     }
-
-    /// <param name="gameidplayerspostrequestcontent"></param>
-    /// <param name="gameId"></param>
-    public IHttpActionResult PostByGameIdPlayers(Models.GameIdPlayersPostRequestContent gameidplayerspostrequestcontent, [FromUri] string gameId) {
-      // TODO: implement PostByGameIdPlayers - route: games/{gameId}/players
-      return Ok();
-    }
-
-  }
 }
